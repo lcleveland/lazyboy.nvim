555a502802598cc394715f1832947bc4aedef344
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-10 10:47:06 -0600

    add null ls

---
+++ lua/plugins/null_ls.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        sources = {
+            null_ls.builtins.formatting.stylua,
+            null_ls.builtins.diagnostics.eslint,
+            null_ls.builtins.completion.spell,
+        },
+    },
+}

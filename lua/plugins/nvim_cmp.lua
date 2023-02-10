7d683805a9e03b7fe740a53da6fe65b819728e90
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-10 17:10:32 -0600

    test

--- lua/plugins/nvim_cmp.lua
+++ lua/plugins/nvim_cmp.lua
@@ -5,6 +5,19 @@ return {
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
         },
+        opts = {
+            mapping = cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs( -4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp'},
+                { name = 'buffer'},
+            }),
+        },
     },
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-buffer",

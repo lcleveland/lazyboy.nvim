40be758cd14e24590aaeff7884ed7901c0f4fc5e
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-10 11:46:42 -0600

    add depend in null_ls

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -1,6 +1,9 @@
 return {
     {
         "jose-elias-alvarez/null-ls.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
         sources = {
             null_ls.builtins.formatting.stylua,
             null_ls.builtins.diagnostics.eslint,

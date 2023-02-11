dbc6ea5d53de50e0d0e1f82e36a7f994f90d3bc8
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-11 17:25:25 -0600

    test

--- lua/plugins/coq_nvim.lua
+++ lua/plugins/coq_nvim.lua
@@ -1,8 +1,11 @@
 return {
     {
         "ms-jpq/coq_nvim",
+        init = function ()
+            vim.cmd([[COQdeps -s]])
+        end,
         config = function ()
             vim.cmd([[COQnow -s]])
-        end
+        end,
     },
 }

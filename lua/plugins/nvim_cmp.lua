6846057877bced99492df98f730a3de6ab8f44a4
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-11 08:40:39 -0600

    config

--- lua/plugins/nvim_cmp.lua
+++ lua/plugins/nvim_cmp.lua
@@ -14,10 +14,8 @@ return {
                     {name = 'snippy'},
                 }),
             })
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            require('lspconfig')['mason-lspconfig'].setup {
-                capabilities = capabilities
-            }
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
         end
     },
 }

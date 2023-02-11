93af2bdc0e3924a95fea2286d945afc50c41c74b
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-11 08:24:04 -0600

    initial config

--- lua/plugins/nvim_cmp.lua
+++ lua/plugins/nvim_cmp.lua
@@ -1,5 +1,19 @@
 return {
     {
         "hrsh7th/nvim-cmp",
+        config = function()
+            local cmp = require('cmp')
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require('snippy').expand_snippet(args.body)
+                    end,
+                },
+                sources = cmp.config.sources({
+                    {name = 'nvim_lsp'},
+                    {name = 'snippy'},
+                }),
+            })
+        end
     },
 }

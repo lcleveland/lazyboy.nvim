9cc33e925b9d5b50f08186023465e81010442f09
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-12 13:15:52 -0600

    add bg color

--- lua/plugins/nvim_notify.lua
+++ lua/plugins/nvim_notify.lua
@@ -2,8 +2,11 @@ return {
     {
         "rcarriga/nvim-notify",
         priority = 100,
-        config = function ()
-           vim.notify = require("notify")
+        config = function()
+            require("notify").setup({
+                background_colour = "#000000",
+            })
+            vim.notify = require("notify")
         end,
     }
 }

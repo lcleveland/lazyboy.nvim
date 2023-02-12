fa04671419b408a41c199ab2e0f67e9b2fb8792c
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-12 15:04:25 -0600

    test

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -4,27 +4,10 @@ return {
         confing = function()
             local remap = vim.api.nvim_set_keymap
             local npairs = require('nvim-autopairs')
-            local Rule = require('nvim-autopairs.rule')
 
-            npairs.setup({
-                check_ts = true,
-                ts_config = {
-                    lua = { 'string' }, -- it will not add a pair on that treesitter node
-                    javascript = { 'template_string' },
-                    java = false, -- don't check treesitter on java
-                },
-                map_bs = false,
-                map_cr = false
-            })
+            npairs.setup({ map_bs = false, map_cr = false })
 
             vim.g.coq_settings = { keymap = { recommended = false } }
-            local ts_conds = require('nvim-autopairs.ts-conds')
-            npairs.add_rules({
-                Rule("%", "%", "lua")
-                :with_pair(ts_conds.is_ts_node({ 'string', 'comment' })),
-                Rule("$", "$", "lua")
-                :with_pair(ts_conds.is_not_ts_node({ 'function' }))
-            })
 
             -- these mappings are coq recommended mappings unrelated to nvim-autopairs
             remap('i', '<esc>', [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })

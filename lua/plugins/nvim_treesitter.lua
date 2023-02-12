6e3c6c4626535e75049c932271e6af80fd409843
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-12 11:19:20 -0600

    test

--- lua/plugins/nvim_treesitter.lua
+++ lua/plugins/nvim_treesitter.lua
@@ -1,6 +1,9 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {"lua","vim","help","python"},
+        },
         config = function ()
             vim.cmd([[TSUpdate]])
         end,

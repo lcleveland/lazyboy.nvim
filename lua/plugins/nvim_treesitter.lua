7cf5d5ffee87496c7ad4ffa56f60a13aca5fd5c6
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-12 11:30:43 -0600

    test

--- lua/plugins/nvim_treesitter.lua
+++ lua/plugins/nvim_treesitter.lua
@@ -3,8 +3,11 @@ return {
         "nvim-treesitter/nvim-treesitter",
         config = function ()
             require("nvim-treesitter.configs").setup {
-                ensure_installed = {"c", "lua", "vim", "help"},
-                highlight = { enable = true },
+                ensure_installed = {"c", "lua", "vim", "help", "python"},
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = true,
+                },
             }
             vim.cmd([[TSUpdate]])
         end,

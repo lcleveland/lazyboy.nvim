e57ed1161c41bdc5f09bca222da4c0fafa1ea8ff
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-12 13:04:31 -0600

    test

--- lua/plugins/nvim_notify.lua
+++ lua/plugins/nvim_notify.lua
@@ -1,6 +1,8 @@
 return {
     {
         "rcarriga/nvim-notify",
+        lazy = false,
+        priority = 100,
         config = function ()
            vim.notify = require("notify")
         end,

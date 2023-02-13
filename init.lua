a5d03cc94c848e05f9e613a9721f81d27ec39b04
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-13 17:48:04 -0600

    telescope and config additions (#23)

--- init.lua
+++ init.lua
@@ -2,12 +2,12 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
-	    "git",
-	    "clone",
-	    "--filter=blob:none",
-	    "https://github.com/folke/lazy.nvim.git",
-	    "--branch=stable", -- latest stable release
-	    lazypath,
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
@@ -18,19 +18,6 @@ vim.g.mapleader = " "
 -- set plugins to use the lua/plugins folder
 require("lazy").setup("plugins")
 
--- line numbers
-vim.wo.number = true
-vim.o.relativenumber = true
-
--- clipboard
-vim.o.clipboard = 'unnamedplus'
-
--- keymaps
-
--- better defaults
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.keymap.set({ 'n', 'v' }, ';', ':')
-
--- word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+-- load user settings
+require("user.keymap")
+require("user.general_settings")
---
+++ lua/user/general_settings.lua
@@ -0,0 +1,6 @@
+-- line numbers
+vim.wo.number = true
+vim.o.relativenumber = true
+
+-- clipboard
+vim.o.clipboard = "unnamedplus"

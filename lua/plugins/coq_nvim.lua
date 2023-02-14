dd638c72265ee0f4c031de0622ac9bdb17b3bba0
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-14 09:47:57 -0600

    coq config

--- lua/plugins/coq_nvim.lua
+++ lua/plugins/coq_nvim.lua
@@ -3,6 +3,11 @@ return {
 		"ms-jpq/coq_nvim",
 		config = function()
 			vim.g.coq_settings = {
+				auto_start = true,
+				tree_sitter = {
+					enabled = true,
+					weight_adjust = 1.0,
+				},
 				display = {
 					pum = {
 						fast_close = false,

3b8677bb449279be7cb9d6e9448fd77866d2c8b6
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-14 10:29:15 -0600

    move from wilder to cmp for : and /

--- lua/plugins/nvim_cmp.lua
+++ lua/plugins/nvim_cmp.lua
@@ -42,6 +42,20 @@ return {
 					{ name = "buffer" },
 				}),
 			})
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+			})
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
 		end,
 	},
 }

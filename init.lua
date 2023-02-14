973adefddf96dd2d31932c14709899123a58f456
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-14 17:22:43 -0600

    move bootstrap to lua module (#32)

--- init.lua
+++ init.lua
@@ -1,19 +1,5 @@
--- bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
--- map leader before plugin configuration
-vim.g.mapleader = " "
+-- bootstrap
+require("bootstrap")
 
 -- set plugins to use the lua/plugins folder
 require("lazy").setup("plugins")

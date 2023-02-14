54e9170340df9b5917407b449546b6430435331c
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-14 15:32:41 -0600

    add keymap

--- lua/user/keymap.lua
+++ lua/user/keymap.lua
@@ -6,10 +6,16 @@ local wk = require("which-key")
 wk.register({
 	f = {
 		name = "Find",
-		f = { "<cmd>Telescope find_files<cr>", "Files"},
-		g = { "<cmd>Telescope live_grep<cr>", "Strings"},
-		b = { "<cmd>Telescope buffers<cr>", "Buffers"},
-		h = { "<cmd>Telescope help_tags<cr>", "Help Tags"},
-		r = { "<cmd>Telescope oldfiles<cr>", "Recent Files"},
-	}
-}, { prefix = "<leader>"})
+		f = { "<cmd>Telescope find_files<cr>", "Files" },
+		g = { "<cmd>Telescope live_grep<cr>", "Strings" },
+		b = { "<cmd>Telescope buffers<cr>", "Buffers" },
+		h = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
+		r = { "<cmd>Telescope oldfiles<cr>", "Recent Files" },
+	},
+	g = {
+		name = "Git",
+		a = { "<cmd>Git add .<cr>", "Add Current File" },
+		c = { "<cmd>Git commit<cr>", "Commit" },
+		p = { "cmd>Git push<cr>", "Push" },
+	},
+}, { prefix = "<leader>" })

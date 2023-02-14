ab15cf8b934f728a93b2cd8b0aa0ecb4102d3550
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-14 16:40:00 -0600

    add required treesitter parser for noice

--- lua/plugins/nvim_treesitter.lua
+++ lua/plugins/nvim_treesitter.lua
@@ -1,21 +1,31 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        priority = 90,
-        config = function ()
-            require("nvim-treesitter.configs").setup {
-                ensure_installed = {"c", "lua", "vim", "help", "python"},
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = true,
-                },
-		rainbow = {
-			enable = true,
-			extended_mode = true,
-			max_file_lines = nil,
-		},
-            }
-        end,
-	build = ":TSUpdate",
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		priority = 90,
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"help",
+					"python",
+					"regex",
+					"bash",
+					"markdown",
+					"markdown_inline",
+				},
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = true,
+				},
+				rainbow = {
+					enable = true,
+					extended_mode = true,
+					max_file_lines = nil,
+				},
+			})
+		end,
+		build = ":TSUpdate",
+	},
 }

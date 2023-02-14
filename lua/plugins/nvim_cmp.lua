d4c8c9f97aed7cd80095dd0c8de9e30cd2be04b0
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-14 11:01:29 -0600

    change cmp config

--- lua/plugins/nvim_cmp.lua
+++ lua/plugins/nvim_cmp.lua
@@ -3,6 +3,7 @@ return {
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
+			local select_opts = { behavior = cmp.SelectBehavior.Select }
 			cmp.setup({
 				snippet = {
 					expand = function(args)
@@ -20,15 +21,18 @@ return {
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 					["<Tab>"] = cmp.mapping(function(fallback)
+						local col = vim.fn.col(".") - 1
 						if cmp.visible() then
-							cmp.select_next_item()
-						else
+							cmp.select_next_item(select_opts)
+						elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
 							fallback()
+						else
+							cmp.complete()
 						end
 					end, { "i", "s" }),
 					["<S-Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
-							cmp.select_prev_item()
+							cmp.select_prev_item(select_opts)
 						else
 							fallback()
 						end

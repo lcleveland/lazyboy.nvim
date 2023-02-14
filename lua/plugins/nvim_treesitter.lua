1ffb8aba502f441ffeefa317d29c4fdb9f11295d
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-13 18:15:25 -0600

    add rainbow ts plugin (#24)

--- lua/plugins/nvim_treesitter.lua
+++ lua/plugins/nvim_treesitter.lua
@@ -9,6 +9,11 @@ return {
                     enable = true,
                     additional_vim_regex_highlighting = true,
                 },
+		rainbow = {
+			enable = true,
+			extended_mode = true,
+			max_file_lines = nil,
+		},
             }
             vim.cmd([[TSUpdate]])
         end,

8877c79595ac6e2983363beb86682d84bbbe51cf
Lyle Cleveland <lcleveland@lselectric.com>
2023-02-15 08:39:16 -0600

    add plugin

---
+++ lua/plugins/neo-tree_nvim.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		config = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+	},
+}

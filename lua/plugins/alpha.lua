808ef4f04730dbc5ab7e3afc34b5d0514566cc5c
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-16 10:03:48 -0600

    alpha config

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -5,28 +5,21 @@ return {
 			local alpha = require("alpha")
 			local dashboard = require("alpha.themes.dashboard")
 
-			-- Set header
 			dashboard.section.header.val = {
-				"                                                     ",
-				"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-				"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-				"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-				"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-				"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-				"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-				"                                                     ",
+				"██╗      █████╗ ███████╗██╗   ██╗██████╗  ██████╗ ██╗   ██╗███╗   ██╗██╗   ██╗██╗███╗   ███╗",
+				"██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██╔══██╗██╔═══██╗╚██╗ ██╔╝████╗  ██║██║   ██║██║████╗ ████║",
+				"██║     ███████║  ███╔╝  ╚████╔╝ ██████╔╝██║   ██║ ╚████╔╝ ██╔██╗ ██║██║   ██║██║██╔████╔██║",
+				"██║     ██╔══██║ ███╔╝    ╚██╔╝  ██╔══██╗██║   ██║  ╚██╔╝  ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║",
+				"███████╗██║  ██║███████╗   ██║   ██████╔╝╚██████╔╝   ██║██╗██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║",
+				"╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═════╝  ╚═════╝    ╚═╝╚═╝╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝",
 			}
 
-			-- Set menu
 			dashboard.section.buttons.val = {
-				dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-				dashboard.button("f", "  > Find file", ":Telescope find_files<CR>"),
-				dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-				dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-				dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+				dashboard.button("SPC F n", "  > New file"),
+				dashboard.button("SPC f f", "  > Find file"),
+				dashboard.button("SPC f r", "  > Recent file"),
 			}
 
-			-- Send config to alpha
 			alpha.setup(dashboard.opts)
 
 			-- Disable folding on alpha buffer

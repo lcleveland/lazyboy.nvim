c95ceb6d517ca2c63b4b33496eb70e78117cf061
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-16 10:21:15 -0600

    config autopairs, change to cmp

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -2,62 +2,26 @@ return {
 	{
 		"windwp/nvim-autopairs",
 		config = function()
-			local remap = vim.api.nvim_set_keymap
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
 			local npairs = require("nvim-autopairs")
 			local Rule = require("nvim-autopairs.rule")
-
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 			npairs.setup({
+				enable_check_bracket_line = true,
+				ignored_next_char = "[%w%.]",
 				check_ts = true,
 				ts_config = {
 					lua = { "string" }, -- it will not add a pair on that treesitter node
 					javascript = { "template_string" },
 					java = false, -- don't check treesitter on java
 				},
-				map_bs = false,
-				map_cr = false,
-				enable_check_bracket_line = true,
-				ignored_next_char = "[%w%.]",
 			})
-
 			local ts_conds = require("nvim-autopairs.ts-conds")
-			vim.g.coq_settings = { keymap = { recommended = false } }
-
-			-- press % => %% only while inside a comment or string
 			npairs.add_rules({
 				Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node({ "string", "comment" })),
 				Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({ "function" })),
 			})
-
-			-- these mappings are coq recommended mappings unrelated to nvim-autopairs
-			remap("i", "<esc>", [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
-			remap("i", "<c-c>", [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
-			remap("i", "<tab>", [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
-			remap("i", "<s-tab>", [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
-
-			-- skip it, if you use another global object
-			_G.MUtils = {}
-
-			MUtils.CR = function()
-				if vim.fn.pumvisible() ~= 0 then
-					if vim.fn.complete_info({ "selected" }).selected ~= -1 then
-						return npairs.esc("<c-y>")
-					else
-						return npairs.esc("<c-e>") .. npairs.autopairs_cr()
-					end
-				else
-					return npairs.autopairs_cr()
-				end
-			end
-			remap("i", "<cr>", "v:lua.MUtils.CR()", { expr = true, noremap = true })
-
-			MUtils.BS = function()
-				if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ "mode" }).mode == "eval" then
-					return npairs.esc("<c-e>") .. npairs.autopairs_bs()
-				else
-					return npairs.autopairs_bs()
-				end
-			end
-			remap("i", "<bs>", "v:lua.MUtils.BS()", { expr = true, noremap = true })
 		end,
 	},
 }
